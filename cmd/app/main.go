f29df2aee9f9736e128a17a8b368e71015e7386e
Oliver Scheer <oliver.scheer@hotmail.de>
2020-12-22 17:06:48 +0000

    tune args

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bingo"
+	"bytes"
 	"flag"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -16,39 +18,57 @@ const defaultIterations int = 1
 
 // TODO: Everything is linear. Always the first player in the row wins with the right numbers
 
-// TODO:
+// Config defines settings for the game
+type Config struct {
+	verbose                    bool
+	numberOfPlayers            int
+	numberOfCardsForEachPlayer int
+	sleepInMS                  int
+	iterations                 int
 
-func main() {
+	// args are the positional (non-flag) command-line arguments.
+	args []string
+}
 
-	// commandline args
-	numberOfPlayers := flag.Int("player", defaultNumberOfPlayer, "number of simulated player")
-	numberOfCardsForEachPlayer := flag.Int("cards", defaultNumberOfCardsForEachPlayer, "number of cards for each player")
-	sleepInMS := flag.Int("sleep", defaultSleepInMS, "delay for each round in milliseconds")
-	iterations := flag.Int("iterations", defaultIterations, "number of interations")
+// parseFlags parses the command-line arguments provided to the program.
+// Typically os.Args[0] is provided as 'progname' and os.args[1:] as 'args'.
+// Returns the Config in case parsing succeeded, or an error. In any case, the
+// output of the flag.Parse is returned in output.
+// A special case is usage requests with -h or -help: then the error
+// flag.ErrHelp is returned and output will contain the usage message.
+func parseFlags(progname string, args []string) (config *Config, output string, err error) {
+	flags := flag.NewFlagSet(progname, flag.ContinueOnError)
+	var buf bytes.Buffer
+	flags.SetOutput(&buf)
 
-	flag.Parse()
+	var conf Config
+	flags.BoolVar(&conf.verbose, "verbose", false, "set verbosity")
+	flags.IntVar(&conf.numberOfPlayers, "player", defaultNumberOfPlayer, "number of simulated player")
+	flags.IntVar(&conf.numberOfCardsForEachPlayer, "cards", defaultNumberOfCardsForEachPlayer, "number of cards for each player")
+	flags.IntVar(&conf.sleepInMS, "sleep", defaultSleepInMS, "delay for each round in milliseconds")
+	flags.IntVar(&conf.iterations, "iterations", defaultIterations, "number of interations")
 
-	// TODO: Add version number here
-	fmt.Println("BinGO v.0.1")
+	err = flags.Parse(args)
+	if err != nil {
+		return nil, buf.String(), err
+	}
+	conf.args = flags.Args()
+	return &conf, buf.String(), nil
+}
 
-	fmt.Println("- player    :", *numberOfPlayers)
-	fmt.Println("- cards     :", *numberOfCardsForEachPlayer)
-	fmt.Println("- sleep     :", *sleepInMS)
-	fmt.Println("- iterations:", *iterations)
-	fmt.Println()
-
-	for iteration := 1; iteration <= *iterations; iteration++ {
+func startGame(config Config) {
+	for iteration := 1; iteration <= config.iterations; iteration++ {
 		fmt.Println("### Iteration", iteration, "####")
 
 		// Initialize real random values
 		rand.Seed(time.Now().UnixNano())
 
 		// Create player
-		var playerList = bingo.CreatePlayer(*numberOfPlayers)
+		var playerList = bingo.CreatePlayer(config.numberOfPlayers)
 
 		settings := bingo.GameSettings{
-			Cards: *numberOfCardsForEachPlayer,
-			Delay: *sleepInMS,
+			Cards: config.numberOfCardsForEachPlayer,
+			Delay: config.sleepInMS,
 		}
 
 		var game = bingo.NewGame(settings)
@@ -59,82 +79,29 @@ func main() {
 		gameData := game.Play()
 
 		fmt.Println(gameData.Winner.Name, "wins.")
-		// var game := bingo.NewGame(iteration)
-		// for p, _ := range &playerList {
-		// 	game.addPlayer(p)
-		// }
-
-		// // Create cards
-		// var numberOfCards = *numberOfPlayers * *numberOfCardsForEachPlayer
-		// var cardList = bingo.CreateCards(numberOfCards)
-
-		// // Distribute cards to player
-		// currentCardID := 0
-		// newCurrentCardID := 0
-		// for idx := range playerList {
-		// 	player := &playerList[idx]
-		// 	for cardID := currentCardID; cardID < (currentCardID + *numberOfCardsForEachPlayer); {
-		// 		player.AddCard(cardList[cardID])
-		// 		cardID++
-		// 		newCurrentCardID = cardID
-		// 	}
-		// 	currentCardID = newCurrentCardID
-		// }
-
-		// // create pot
-		// var pot []int
-		// for ball := 0; ball < 75; ball++ {
-		// 	var alreadyInPot bool = true
-		// 	var newNumber int
-		// 	for alreadyInPot {
-		// 		newNumber = rand.Intn(75) + 1
-		// 		if ball == 0 {
-		// 			alreadyInPot = false
-		// 			break
-		// 		}
-		// 		var found bool = false
-		// 		for i := 0; i < ball; i++ {
-		// 			if pot[i] == newNumber {
-		// 				found = true
-		// 				break
-		// 			}
-		// 		}
-		// 		if !found {
-		// 			alreadyInPot = false
-		// 		}
-		// 	}
-		// 	pot = append(pot, ball)
-		// 	pot[ball] = newNumber
-		// }
-
-		// // linear way: get number and send it to player
-		// win := false
-		// for round := 1; round <= 75; round++ {
-		// 	ms := time.Duration(*sleepInMS)
-		// 	time.Sleep(ms * time.Millisecond)
-		// 	currentNumber := pot[round-1]
-		// 	fmt.Println("Round: ", round, " - Number: ", currentNumber)
-		// 	for idx := range playerList {
-		// 		player := &playerList[idx]
-		// 		win = player.CheckNumber(currentNumber)
-		// 		if win {
-		// 			break
-		// 		}
-		// 	}
-		// 	if win {
-		// 		break
-		// 	}
-		// }
-
-		// parallel way: get number and send it to player
-		// for round := 1; round <= 75; round++ {
-		// 	time.Sleep(sleepInMS * time.Millisecond)
-		// 	currentNumber := pot[round-1]
-		// 	fmt.Println("Round: ", round, " - Number: ", currentNumber)
-		// 	for idx := range playerList {
-		// 		player := &playerList[idx]
-		// 		player.CheckNumber(currentNumber)
-		// 	}
-		// }
 	}
+
+}
+
+func main() {
+
+	// checking args
+	config, output, err := parseFlags(os.Args[0], os.Args[1:])
+	if err == flag.ErrHelp {
+		fmt.Println(output)
+		os.Exit(2)
+	} else if err != nil {
+		fmt.Println("got error:", err)
+		fmt.Println("output:\n", output)
+		os.Exit(1)
+	}
+
+	fmt.Println("BinGO v.0.1")
+	fmt.Println("- player    :", config.numberOfPlayers)
+	fmt.Println("- cards     :", config.numberOfCardsForEachPlayer)
+	fmt.Println("- sleep     :", config.sleepInMS)
+	fmt.Println("- iterations:", config.iterations)
+	fmt.Println()
+
+	startGame(*config)
 }
